d91759400b798cd6cc873e2a00d3cdfb54b70313
Nikhilsahu31 <143781231+Nikhilsahu31@users.noreply.github.com>
2024-09-11 21:58:47 +0530

    add basic react file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-yellow-400 font-extrabold text-3xl text-center uppercase'>hello from react redux-toolkit</h1>
+    </>
+  )
+}
+
+export default App
